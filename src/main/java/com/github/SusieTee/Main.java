c5af1539a58e72e9d528652a432eee16c9e00158
SusieTee <t.h.s.98.hotmail.com@gmail.com>
2023-11-01 15:20:25 +0800

    Add message

--- src/main/java/com/github/SusieTee/Main.java
+++ src/main/java/com/github/SusieTee/Main.java
@@ -5,5 +5,6 @@ package com.github.SusieTee;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello ,World!");
+        System.out.printf("Hi from develop");
         }
     }
